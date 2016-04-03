c8808739f9d68208e7be4785175f753d657643eb
David Mendez <desiratech@gmail.com>
2016-04-02 23:32:51 -0500

    sprite animation

--- src/engine.ts
+++ src/engine.ts
@@ -13,13 +13,25 @@ export default class Engine {
     
     static preload() {
         Engine.game.load.image('terrain', 'assets/terrain_atlas.png');
+        Engine.game.load.spritesheet('seikho', 'assets/seikho.png', 32, 32);
+        Engine.game.load.spritesheet('catRunning', 'assets/runningcat.png', 512, 256, 8);
+        
+        
     }
     
     static create() {
+        Engine.game.add.sprite(0, 0, 'seikho');
+        var cat = Engine.game.add.sprite(100, 250, 'catRunning');
+        
+        var run = cat.animations.add('run');
+        
+        cat.animations.play('run', 15, true); 
+        
         Engine.game.math.snapToFloor(1, 32);
     }
     
     static update() {
         
     }
+    
 }
