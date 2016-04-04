84ef797b89b020689bcf54b8e5143ed85db5f4cb
David Mendez <desiratech@gmail.com>
2016-04-03 22:01:54 -0500

    added assest - seikho can MOVE!

--- src/engine.ts
+++ src/engine.ts
@@ -7,6 +7,7 @@
 export default class Engine {
     static player;
     static platforms;
+    static cursors;
 
     static game = new Phaser.Game(800, 600, Phaser.AUTO, 'content', {
         preload: Engine.preload,
@@ -19,7 +20,7 @@ export default class Engine {
         Engine.game.load.image('ground', 'assets/platform.png');
         Engine.game.load.image('star', 'assets/star.png');
         Engine.game.load.image('dude', 'assets/dude.png');
-        Engine.game.load.spritesheet('catRunning', 'assets/runningcat.png', 512, 256, 8);
+        Engine.game.load.spritesheet('seikho', 'assets/seikho.png', 32, 32, 12);
     }
 
     static create() {
@@ -44,8 +45,7 @@ export default class Engine {
         ledge = Engine.platforms.create(-150, 250, 'ground');
         ledge.body.immovable = true;
 
-        Engine.player = Engine.game.add.sprite(32, Engine.game.world.height - 150, 'catRunning');
-        Engine.player.scale.setTo(.3, .3);
+        Engine.player = Engine.game.add.sprite(32, Engine.game.world.height - 150, 'seikho');
 
         Engine.game.physics.arcade.enable(Engine.player);
 
@@ -53,15 +53,35 @@ export default class Engine {
         Engine.player.body.gravity.y = 300;
         Engine.player.body.collideWorldBounds = true;
 
-        var run = Engine.player.animations.add('run');
-        Engine.player.animations.play('run', 15, true);
+        var run = Engine.player.animations.add('right', [7,8], 10, true);
+        var run = Engine.player.animations.add('left', [10,11], 10, true);
         
+
         Engine.game.math.snapToFloor(1, 32);
     }
 
     static update() {
 
         Engine.game.physics.arcade.collide(Engine.player, Engine.platforms);
+
+        Engine.cursors = Engine.game.input.keyboard.createCursorKeys();
+         //  Reset the players velocity (movement)
+        Engine.player.body.velocity.x = 0;
+        
+        if (Engine.cursors.left.isDown) {
+            Engine.player.body.velocity.x = -150;
+            Engine.player.animations.play('left');
+        } else if (Engine.cursors.right.isDown) {
+            Engine.player.body.velocity.x = 150;
+            Engine.player.animations.play('right');
+        } else {
+            Engine.player.animations.stop();
+            Engine.player.frame = 2;
+        }
+        
+        if(Engine.cursors.up.isDown && Engine.player.body.touching.down){
+            Engine.player.body.velocity.y = -350;
+        }
 
     }
 
