679a50ca9e7c41d0f58bfe16621a3abeea7bce88
Carl <carl@carlossus.com>
2016-04-04 22:31:25 +0800

    Separate logic from Engine into separate module

--- src/engine.ts
+++ src/engine.ts
@@ -1,88 +1,64 @@
-// Phaser is available globally via index.html
-
-/**
- * Singleton.. Why the hell would we want multiple of this monstrosity
- */
+import init from './game';
+import Player from './player';
+import * as inits from './initialisers';
 
 export default class Engine {
-    static player;
-    static platforms;
-    static cursors;
+    constructor() {
+        inits.creates.push(this.create);
+        inits.updates.push(this.update);
 
-    static game = new Phaser.Game(800, 600, Phaser.AUTO, 'content', {
-        preload: Engine.preload,
-        create: Engine.create,
-        update: Engine.update
-    });
-
-    static preload() {
-        Engine.game.load.image('sky', 'assets/sky.png');
-        Engine.game.load.image('ground', 'assets/platform.png');
-        Engine.game.load.image('star', 'assets/star.png');
-        Engine.game.load.image('dude', 'assets/dude.png');
-        Engine.game.load.spritesheet('seikho', 'assets/seikho.png', 32, 32, 12);
+        const game = init();
+        this.game = game;
     }
 
-    static create() {
+    player: Phaser.Sprite;
+    platforms: Phaser.Group;
+    cursors: Phaser.CursorKeys;
+    game: Phaser.Game;
 
-        Engine.game.physics.startSystem(Phaser.Physics.ARCADE);
+    create = () => {
+        const platforms = this.game.add.group();
+        platforms.enableBody = true;
+        this.platforms = platforms;
 
-        Engine.game.add.sprite(0, 0, 'sky');
-        Engine.game.add.sprite(0, 0, 'star');
-
-        Engine.platforms = Engine.game.add.group();
-        Engine.platforms.enableBody = true;
-
-        var ground = Engine.platforms.create(0, Engine.game.world.height - 64, 'ground');
-
+        const ground = platforms.create(0, this.game.world.height - 64, 'platform');
         ground.scale.setTo(2, 2);
         ground.body.immovable = true;
 
+        const highLedge = platforms.create(400, 400, 'platform');
+        highLedge.body.immovable = true;
 
-        var ledge = Engine.platforms.create(400, 400, 'ground');
+        const lowLedge = platforms.create(-150, 250, 'platform');
+        lowLedge.body.immovable = true;
 
-        ledge.body.immovable = true;
-        ledge = Engine.platforms.create(-150, 250, 'ground');
-        ledge.body.immovable = true;
-
-        Engine.player = Engine.game.add.sprite(32, Engine.game.world.height - 150, 'seikho');
-
-        Engine.game.physics.arcade.enable(Engine.player);
-
-        Engine.player.body.bounce.y = .2;
-        Engine.player.body.gravity.y = 300;
-        Engine.player.body.collideWorldBounds = true;
-
-        var run = Engine.player.animations.add('right', [7,8], 10, true);
-        var run = Engine.player.animations.add('left', [10,11], 10, true);
-        
-
-        Engine.game.math.snapToFloor(1, 32);
+        const playerOpts = {
+            game: this.game,
+            sprite: 'seikho',
+            coordinate: { x: 32, y: this.game.world.height - 150 }
+        }
+        this.player = new Player(playerOpts).player;
     }
 
-    static update() {
+    update = () => {
+        this.game.physics.arcade.collide(this.player, this.platforms);
+        const cursors = this.game.input.keyboard.createCursorKeys();
 
-        Engine.game.physics.arcade.collide(Engine.player, Engine.platforms);
+        //  Reset the players velocity (movement)
+        this.player.body.velocity.x = 0;
 
-        Engine.cursors = Engine.game.input.keyboard.createCursorKeys();
-         //  Reset the players velocity (movement)
-        Engine.player.body.velocity.x = 0;
-        
-        if (Engine.cursors.left.isDown) {
-            Engine.player.body.velocity.x = -150;
-            Engine.player.animations.play('left');
-        } else if (Engine.cursors.right.isDown) {
-            Engine.player.body.velocity.x = 150;
-            Engine.player.animations.play('right');
+        if (cursors.left.isDown) {
+            this.player.body.velocity.x = -150;
+            this.player.animations.play('left');
+        } else if (cursors.right.isDown) {
+            this.player.body.velocity.x = 150;
+            this.player.animations.play('right');
         } else {
-            Engine.player.animations.stop();
-            Engine.player.frame = 2;
-        }
-        
-        if(Engine.cursors.up.isDown && Engine.player.body.touching.down){
-            Engine.player.body.velocity.y = -350;
+            this.player.animations.stop();
+            this.player.frame = 2;
         }
 
+        if (cursors.up.isDown && this.player.body.touching.down) {
+            this.player.body.velocity.y = -350;
+        }
     }
-
 }
